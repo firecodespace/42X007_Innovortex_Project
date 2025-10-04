701b7420c0ab8a0ec94ea25102c39b9f08925489
Yash Bendersh <yashbendresh2266@gmail.com>
2025-10-04 23:39:19 +0530

    Initial project setup with SFML and ImGui-SFML
    Added initial source files, CMake configuration, and project structure for GladiatorSFML. Integrated SFML and ImGui-SFML as dependencies and included generated build and configuration files.

---
+++ GladiatorSFML/GladiatorSFML/GladiatorSFML.cpp
@@ -0,0 +1,12 @@
+﻿// GladiatorSFML.cpp : Defines the entry point for the application.
+//
+
+#include "GladiatorSFML.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
